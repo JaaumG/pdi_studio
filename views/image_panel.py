a5d9e40549a4d1974aa78a7b054908ca1ee5f332
JaaumG <mail@joaoguilherme.dev>
2025-11-03 19:31:43 -0300

    feat(image_panel.py): adiciona painel para exibir imagens originais e processadas

--- views/image_panel.py
+++ views/image_panel.py
@@ -1,13 +1,29 @@
 import tkinter as tk
-from tkinter import Label
+from tkinter import Label, Frame
 
 class ImagePanel:
     def __init__(self, root):
-        self.frame = tk.Frame(root, bg="#222")
-        self.label = Label(self.frame, bg="#222")
-        # Preenche o espaço disponível do painel, sem alterar o tamanho do painel de controle
-        self.label.pack(fill="both", expand=True)
+        self.frame = Frame(root, bg="#222")
 
-    def show_image(self, image):
-        self.label.config(image=image)
-        self.label.image = image  # mantém referência
+        self.original_frame = Frame(self.frame, bg="#222", relief=tk.SUNKEN, bd=1)
+        self.original_frame.pack(side=tk.LEFT, fill="both", expand=True, padx=5, pady=5)
+
+        Label(self.original_frame, text="Original", bg="#222", fg="white").pack(pady=2)
+        self.original_label = Label(self.original_frame, bg="#222")
+        self.original_label.pack(fill="both", expand=True)
+
+        self.processed_frame = Frame(self.frame, bg="#222", relief=tk.SUNKEN, bd=1)
+        self.processed_frame.pack(side=tk.RIGHT, fill="both", expand=True, padx=5, pady=5)
+
+        Label(self.processed_frame, text="Processada", bg="#222", fg="white").pack(pady=2)
+        self.processed_label = Label(self.processed_frame, bg="#222")
+        self.processed_label.pack(fill="both", expand=True)
+
+
+    def show_original_image(self, image):
+        self.original_label.config(image=image)
+        self.original_label.image = image
+
+    def show_processed_image(self, image):
+        self.processed_label.config(image=image)
+        self.processed_label.image = image
