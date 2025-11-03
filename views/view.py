208ef4c7ed00286e22c8d45edd91a4a3f05f6e3f
JaaumG <mail@joaoguilherme.dev>
2025-11-03 19:31:59 -0300

    feat(view.py): integra ControlPanel com instância do controller e adiciona método para exibir imagem original

--- views/view.py
+++ views/view.py
@@ -1,4 +1,3 @@
-import tkinter as tk
 from views.menu_bar import MenuBar
 from views.image_panel import ImagePanel
 from views.control_panel import ControlPanel
@@ -13,15 +12,19 @@ class View:
         self.root.config(menu=self.menu.menubar)
 
         # Painéis
+        # Passa a instância do controller para o control_panel
+        self.control_panel = ControlPanel(self.root, controller)
         self.image_panel = ImagePanel(self.root)
-        self.control_panel = ControlPanel(self.root)
 
         # Prioriza empacotar o painel de controle primeiro para reservar espaço do log
         self.control_panel.frame.pack(side="right", fill="y")
         self.image_panel.frame.pack(side="left", fill="both", expand=True)
 
+    def display_original_image(self, image):
+        self.image_panel.show_original_image(image)
+
     def display_image(self, image):
-        self.image_panel.show_image(image)
+        self.image_panel.show_processed_image(image)
 
     def log_action(self, text):
         self.control_panel.add_log(text)
